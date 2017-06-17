84296b8f79644ae9d7a7c2cdbfecc994081c9ada
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-17 17:46:22 +0300

    creatin StackoverflowWebsite

--- src/main/java/ua/sergey/test/service/StackoverflowService.java
+++ src/main/java/ua/sergey/test/service/StackoverflowService.java
@@ -9,7 +9,10 @@ import java.util.List;
 @Service
 public class StackoverflowService {
     private static List<StackoverflowWebsite> items = new ArrayList<>();
-    static 
+    static {
+        items.add(new StackoverflowWebsite("stackoverflow", "http://stackoverflow.com", "http://www.iconsdb.com/icons/download/orange/stackoverflow-6-64.ico", "Stack Overflow", "for programmers"));
+        items.add(new StackoverflowWebsite("superuser", "http://superuser.com", "http://www.iconsdb.com/icons/download/color/CFFF30/linkedin-3-64.ico", "Super User", "for power user"));
+    }
 
     public List<StackoverflowWebsite> findAll() {
         return items;
