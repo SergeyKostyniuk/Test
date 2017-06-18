39c7357868ea46c1d6583d29757f93feb82439e7
Sergey Kostyniuk <Sergey7kos@gmail.com>
2017-06-19 00:16:01 +0300

    somes fixs

--- src/main/java/ua/sergey/test/service/StackExchangeClient.java
+++ src/main/java/ua/sergey/test/service/StackExchangeClient.java
@@ -1,6 +1,9 @@
 package ua.sergey.test.service;
 
+import org.apache.http.client.HttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.springframework.http.client.ClientHttpRequestFactory;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
 import ua.sergey.test.model.SiteDTO;
@@ -11,9 +14,10 @@ import java.net.URISyntaxException;
 import java.util.List;
 
 @Component
-public class StackExchangeClient
-
-    private RestTemplate restTemplate = new RestTemplate();
+public class StackExchangeClient {
+    HttpClient httpClient = HttpClientBuilder.create().build();
+    ClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory(httpClient);
+    private RestTemplate restTemplate = new RestTemplate(requestFactory);
 
     public List<SiteDTO> getSites() {
         String url = "https://api.stackexchange.com/2.2/sites?page=1&pagesize=9999&filter=!8IeYYZbP7kF8WkCg(i*zE";
